1394a96bef81fa5ea83dc2a0cccdce8bc688b5e7
mutumoto630 <ak-matz@outlook.jp>
2020-06-26 15:43:19 +0900

    sampleをアップデート

--- sample.ts
+++ sample.ts
@@ -1,4 +1,6 @@
-let message1 : string = "hello1";
-console.log(message1);
-let message2 : string = "hello2";
-console.log(message2);
+let message1 : string = "hello1"
+console.log(message1)
+let message2 : string = "hello2"
+console.log(message2)
+let message3 : string = "hello3"
+console.log(message3)
